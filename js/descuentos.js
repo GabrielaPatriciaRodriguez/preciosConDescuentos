428a5de06037e966deab946746a80df20c330955
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2022-01-16 09:48:17 -0300

    descuento con cupon

--- js/descuentos.js
+++ js/descuentos.js
@@ -17,3 +17,38 @@ function onClickPrecioConDescuento(){
     const resultP = document.getElementById("resultP");
     resultP.innerText = "El precio con descuento es $" + precioDescuento;
 }
+
+const cupones = [
+    "DESCUENTO15",
+    "DESCUENTO30",
+    "DESCUENTO25",
+];
+
+function onClickDescuentoConCupon() {
+    const inputPrecio = document.getElementById("inputPrecio");
+    const precioValue = inputPrecio.value;
+    
+    const inputCupon = document.getElementById("inputCupon");
+    const cuponValue = Number(inputCupon.value);
+  
+    let descuento;
+  
+    switch(cuponValue) {
+      case cupones[0]: // "DESCUENTO15"
+        descuento = 15;
+      break;
+      case cupones[1]: // "DESCUENTO30"
+        descuento = 30;
+      break;
+      case cupones[2]: // "DESCUENTO25"
+        descuento = 25;
+      break;
+    }
+  
+  
+    const precioDesc = calcularPrecioConDescuento(precioValue, descuento);
+
+  
+    const result = document.getElementById("result");
+    result.innerText = "El precio con descuento son: $" + precioDesc;
+  }
